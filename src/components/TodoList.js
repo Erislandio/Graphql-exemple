3a6dfa08940b67b1e07fb0c16f1129d60a2ee174
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-17 23:06:18 -0200

    Adição da query de consulta de todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+// ! Query quando vamos consutar os dados da api
+// * Mutation quando vamos mudar algo na api
+// todo => tipo subscription quando queremos 'ouvir' nossa api em tempo real
+
+
 class TodoList extends Component {
-    render() {
+
+    renderTodoList = () => {
         return (
             <ul>
-                <il>
-                    teste
-                </il>
-                <il>
-                    teste
-                </il>
-                <il>
-                    teste
-                </il>
+                {this.props.todos.allTodoes.map(todo => (
+                    <li key={todo.id}>
+                        {todo.text}
+                    </li>
+                ))
+                }
             </ul>
+        );
+    }
+
+    render() {
+        console.log(this.props);
+        const { todos } = this.props;
+        return (
+            <div>
+                {
+                    todos.loading ? (<p>Carregando...</p>)
+                        : this.renderTodoList()
+                }
+            </div>
         );
     }
 }
 
-export default TodoList;
+
+const TodosQuery = gql`
+    query {
+        allTodoes{
+            id
+            text
+        }
+    }
+`;
+
+export default graphql(TodosQuery,
+    { name: 'todos' }
+)(TodoList);
